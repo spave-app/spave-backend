15eeb94f3a4bb377ea59ab5449a76e87d4072a6b
TheBester14 <40973175+TheBester14@users.noreply.github.com>
2026-03-10 21:02:46 -0400

    Spring boot init

---
+++ src/main/java/com/spave/backend/spave/SpaveApplication.java
@@ -0,0 +1,13 @@
+package com.spave.backend.spave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spave/backend/spave/SpaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spave.backend.spave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
